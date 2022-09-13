6b270b88204500219a8f28b891275f4a3636ce7b
cesarsnoronha <cesarsnoronha@gmail.com>
2022-09-13 20:36:50 -0300

    limpando o codigo

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 usuario_ativo = 'nao'
 
-# Funções do menu
 
+# Funções do menu
 def menu_login():
     rodando_menu = True
     while rodando_menu:
-        opcao_seguida = input(f'Realizar login (Escreva 1)\n' #Feito
+        opcao_seguida = input(f'o----------------------------------------------------------o\n'
+                              f'Realizar login (Escreva 1)\n' #Feito
                               f'Criar uma conta (Escreva 2)\n'
-                              f'Fechar (Escreva 3)\n' #Feito
+                              f'Fechar (Escreva 3)\n'
+                              f'o----------------------------------------------------------o\n' #Feito
                               f'Selecione uma das opções acima digitando apenas o numero: ')
 
         try:
@@ -38,14 +40,15 @@ def menu_login():
             return opcao_seguida
         else:
             print(f'Espera-se uma opcao_seguida com valor inteiro entre 1 e 3\n')
-
 def menu_user():
     rodando_menu = True
     while rodando_menu:
-        opcao_seguida = input(f'Buscar um filme (Escreva 1)\n' #Feito
-                              f'Editar review para um filme (Escreva 2)\n' #TODO A fazer <----
-                              f'Consultar overview (Escreva 3)\n' #Feito
-                              f'Sair (Escreva 4)\n' #Feito
+        opcao_seguida = input(f'o----------------------------------------------------------o\n'
+                              f'Buscar um filme (Escreva 1)\n'
+                              f'Editar review para um filme (Escreva 2)\n'
+                              f'Consultar overview (Escreva 3)\n'
+                              f'Sair (Escreva 4)\n'
+                              f'o----------------------------------------------------------o\n'
                               f'Selecione uma das opções acima digitando apenas o numero: ')
 
         try:
@@ -74,14 +77,15 @@ def menu_user():
 
         else:
             print(f'Espera-se uma opcao_seguida com valor inteiro entre 1 e 5\n')
-
 def menu_adm():
     rodando_menu = True
     while rodando_menu:
-        opcao_seguida = input(f'Buscar um usuário (Escreva 1)\n'
+        opcao_seguida = input(f'o----------------------------------------------------------o\n'
+                              f'Buscar um usuário (Escreva 1)\n'
                               f'Deletar um usuário (Escreva 2)\n'
                               f'Adicionar um usuário (Escreva 3)\n'
                               f'Sair (Escreva 4)\n'
+                              f'o----------------------------------------------------------o\n'
                               f'Selecione uma das opções acima digitando apenas o numero: ')
 
         try:
@@ -110,6 +114,7 @@ def menu_adm():
         else:
             print(f'Espera-se uma opcao_seguida com valor inteiro entre 1 e 4\n')
 
+
 # Classes
 class usuario:
     def __init__(self, name, senha, id, movies_reviewed):
@@ -117,10 +122,10 @@ class usuario:
         self._id = id
         self._movies_reviewed = movies_reviewed
         self._senha = senha
-
 class adm(usuario):
     def print_funcionarios(data_users):
         print(data_users)
+
 
 # Funções relacionadas ao login
 def login():  # return usuario  #usuario_ativo = login()
@@ -133,7 +138,6 @@ def login():  # return usuario  #usuario_ativo = login()
 
             return item_usuario
     print(f'Usuário não cadastrado')
-
 def buscar_usuario():
     usuario = input(f'Digite o usuário para busca: ')
     usuarios_cadastrados = lendo_users_json()
@@ -145,7 +149,6 @@ def buscar_usuario():
 def logout():  # usuario_ativo = 'nao' #vai para o menu_login
     usuario = 'nao'
     return menu_login()
-
 def delete_user():
     usuarios_cadastrados = lendo_users_json()
     passou_checagem = True
@@ -160,8 +163,6 @@ def delete_user():
             salvando_users_json(usuarios_cadastrados)
             print(f'usuário delatado')
             break
-
-
 def create_account():  # return usuario  #usuario_ativo = login()
     usuario = input(f'Digite seu usuário: ')
     usuarios_cadastrados = lendo_users_json()
@@ -183,7 +184,6 @@ def create_account():  # return usuario  #usuario_ativo = login()
 
 
 # Funções relacionadas aos filmes
-
 def consultando_filme():  # return opcao_seguida
     rodando_menu = True
     while rodando_menu:
@@ -198,11 +198,9 @@ def consultando_filme():  # return opcao_seguida
 
         if it_is and 1 <= opcao_seguida <= tamanho:
             rodando_menu = False
-            imprimir_filme(opcao_seguida)
             return opcao_seguida
         else:
             print(f'Espera-se uma opção seguida com valor inteiro entre 1 e {tamanho}\n')
-
 def imprimir_filme(opcao_seguida):
     titulo = data[opcao_seguida]['Series_Title']
     ano = data[opcao_seguida]['Released_Year']
@@ -213,7 +211,6 @@ def imprimir_filme(opcao_seguida):
 
     print(f'O filme escolhido é o filme {titulo}, esse é um filme de {ano}, produzido por {director}.\n'
           f'Seus generos são {genero} e sua nota no IMDB é {nota} com {votos} votos')
-
 def imprimir_overview_filme(opcao_seguida):
     titulo = data[opcao_seguida]['Series_Title']
     overview = data[opcao_seguida]['Overview']
@@ -262,7 +259,6 @@ def editando_filme(filme_seguido):
             return filme_seguido
         else:
             print(f'Espera-se uma opcao_seguida com valor inteiro entre 1 e 6\n')
-
 def add_review(filme_seguido):
     nota = data[filme_seguido]['Metrics']['IMDB_Rating']
     n_votos = data[filme_seguido]['Metrics']["No_of_Votes"]
@@ -287,25 +283,23 @@ def add_review(filme_seguido):
         else:
             print(f'Espera-se uma opcao_seguida com valor inteiro entre 0 e 10\n')
 
+
 # Funções relacionadas ao json
 def lendo_filmes_json():  # return data
     f = open('imdb_top_1000.json')  # Opening JSON file
     data = json.load(f)  # returns JSON object as a dictionary
     f.close()
     return data
-
 def salvando_filmes_json(data):
     with open('imdb_top_1000.json', "w") as write_file:
         json.dump(data, write_file)
         write_file.close()
     print('Você salvou seu arquivo json')
-
 def lendo_users_json():
     f = open('users.json')  # Opening JSON file
     data_users = json.load(f)  # returns JSON object as a dictionary
     f.close()
     return data_users
-
 def salvando_users_json(data):
     with open('users.json', "w") as write_file:
         json.dump(data, write_file)
